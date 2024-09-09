fa734523546b87ba6de1f1b83f66fb0cfd43ef54
David Cañadas Mazo <david.canadas.mazo@gmail.com>
2024-09-09 22:06:11 +0200

    PRRC: Fix 3D View Z-order when coming from fullscreen.

--- SqrMelon/main.py
+++ SqrMelon/main.py
@@ -117,7 +117,7 @@ class App(QMainWindowState):
         viewDock = self._addDockWidget(window, '3D View', where=Qt.DockWidgetArea.TopDockWidgetArea)
         self.__sceneView.setDockWidget(viewDock)
         self.__viewDock = viewDock  # Need this for F11 feature
-        self.__restoreFullScreenInfo: Optional[tuple[QSize, bool]] = None
+        self.__restoreFullScreenInfo: Optional[tuple[QSize, bool, bool]] = None
         logDock = self._addDockWidget(PyDebugLog.create(), 'Python log', where=Qt.DockWidgetArea.TopDockWidgetArea)
         self.tabifyDockWidget(logDock, viewDock)
 
@@ -373,7 +373,7 @@ class App(QMainWindowState):
 
         if not dockWidget.isFullScreen():
             floating = dockWidget.isFloating()
-            self.__restoreFullScreenInfo = dockWidget.size(), floating
+            self.__restoreFullScreenInfo = dockWidget.size(), floating, dockWidget.visibleRegion().isEmpty()
             if not floating:
                 dockWidget.setFloating(True)
             dockWidget.showFullScreen()
@@ -384,6 +384,8 @@ class App(QMainWindowState):
                 dockWidget.setFloating(self.__restoreFullScreenInfo[1])
             if dockWidget.isFloating():
                 dockWidget.resize(self.__restoreFullScreenInfo[0])
+            if not self.__restoreFullScreenInfo[2]:
+                dockWidget.raise_()
 
     def __toggleUILock(self, state: bool) -> None:
         gSettings.setValue('lockui', '1' if state else '0')
